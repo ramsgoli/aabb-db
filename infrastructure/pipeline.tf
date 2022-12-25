7ad03bee1311a332108f2fcdfe31a1126e9ac7f0
Ram Goli <ramsgoli@gmail.com>
2022-12-25 13:44:31 -0800

    update pipelines

--- infrastructure/pipeline.tf
+++ infrastructure/pipeline.tf
@@ -1,5 +1,10 @@
 resource "buildkite_pipeline" "pipeline" {
   name       = "columnar_pipeline"
   repository = "git@github.com:ramsgoli/columnar_store"
-  steps      = file("../.buildkite/pipeline.yml")
+  steps      = <<-EOT
+    steps:
+      - label: ":buildkite: Upload Pipeline"
+        command: |
+          buildkite-agent pipeline upload
+  EOT
 }
